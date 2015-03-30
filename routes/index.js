a81c049acf298ab92e88dca6b0e9e11ada4e6c32
Derin Dutz <derindutz@rescomp-14-290481.stanford.edu>
2015-03-30 01:12:46 -0700

    updated routes

--- routes/index.js
+++ routes/index.js
@@ -11,16 +11,6 @@ router.get('/team', function(req, res) {
     res.render('team', { title: 'Team' });
 });
 
-/* GET projects page. */
-router.get('/projects', function(req, res) {
-    res.render('projects', { title: 'Projects' });
-});
-
-/* GET hackathon page. */
-router.get('/hackathon', function(req, res) {
-  res.render('hackathon', { title : 'Hackathon' });
-});
-
 /* GET contact page. */
 router.get('/contact', function(req, res) {
   res.render('contact', { title : 'Contact' });
